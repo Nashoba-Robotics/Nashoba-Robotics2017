e78657399ee89276d82ee68c1e4af04cb8c86d6c
gbear605 <garrisontaylor@gmail.com>
2017-01-29 14:23:45 -0500

    Add some more commands

---
+++ RoboRIO/src/edu/nr/robotics/subsystems/drive/DriveSetPIDSmartDashboardCommand.java
@@ -0,0 +1,23 @@
+package edu.nr.robotics.subsystems.drive;
+
+import edu.nr.lib.NRCommand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class DriveSetPIDSmartDashboardCommand extends NRCommand {
+
+
+	@Override
+	protected void onStart() {
+		SmartDashboard.putNumber("Drive P", SmartDashboard.getNumber("Drive P", Drive.P));
+		SmartDashboard.putNumber("Drive I", SmartDashboard.getNumber("Drive I", Drive.I));
+		SmartDashboard.putNumber("Drive D", SmartDashboard.getNumber("Drive D", Drive.D));
+		SmartDashboard.putNumber("Drive F", SmartDashboard.getNumber("Drive F", Drive.F));
+		
+		Drive.getInstance().setPID(SmartDashboard.getNumber("Drive P", Drive.P), 
+								   SmartDashboard.getNumber("Drive I", Drive.I), 
+								   SmartDashboard.getNumber("Drive D", Drive.D), 
+								   SmartDashboard.getNumber("Drive F", Drive.F));
+	}
+    
+}
+
---
+++ RoboRIO/src/edu/nr/robotics/subsystems/drive/DriveTurnConstantSmartDashboardSpeedCommand.java
@@ -0,0 +1,32 @@
+package edu.nr.robotics.subsystems.drive;
+
+import edu.nr.lib.NRCommand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class DriveTurnConstantSmartDashboardSpeedCommand extends NRCommand {
+	
+	double turnSpeed;
+	
+	String turnSpeedString;
+	
+	double defaultTurnSpeed;
+	
+	public DriveTurnConstantSmartDashboardSpeedCommand(String turnSpeedString, double defaultTurnSpeed) {
+		super(Drive.getInstance());		
+		
+		this.turnSpeedString = turnSpeedString;
+		
+		this.defaultTurnSpeed = defaultTurnSpeed;
+	}
+	
+	@Override
+	public void onStart() {
+		turnSpeed = SmartDashboard.getNumber(turnSpeedString, defaultTurnSpeed);
+	}
+	
+	@Override
+	public void onExecute() {
+		Drive.getInstance().arcadeDrive(0, turnSpeed);
+	}
+	
+}
