6fecedd5171f783e2316495e364998bad7fee2ad
gbear605 <garrisontaylor@gmail.com>
2017-02-17 17:38:36 -0500

    Add loader reverse command TODO

--- RoboRIO/src/edu/nr/robotics/subsystems/loader/LoaderReverseCommand.java
+++ RoboRIO/src/edu/nr/robotics/subsystems/loader/LoaderReverseCommand.java
@@ -6,6 +6,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class LoaderReverseCommand extends CommandGroup{
 	
 	public LoaderReverseCommand() {
+		//TODO: Loader: Reverse command should run the agitator in reverse
 		addSequential(new LoaderSpeedCommand(RobotMap.LOADER_REVERSE_SPEED));
 	}
 }
