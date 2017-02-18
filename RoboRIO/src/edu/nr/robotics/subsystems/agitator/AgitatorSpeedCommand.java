bd925cffa79d969a4d31c6b4bf4e3e0b38e0c134
gbear605 <garrisontaylor@gmail.com>
2017-02-18 10:19:58 -0500

    Add AgitatorSpeedCommand

---
+++ RoboRIO/src/edu/nr/robotics/subsystems/agitator/AgitatorSpeedCommand.java
@@ -0,0 +1,19 @@
+package edu.nr.robotics.subsystems.agitator;
+
+import edu.nr.lib.commandbased.NRCommand;
+import edu.nr.robotics.subsystems.loader.Loader;
+
+public class AgitatorSpeedCommand extends NRCommand {
+
+	double percent;
+	
+	public AgitatorSpeedCommand(double percent) {
+		super(Agitator.getInstance());
+		this.percent = percent;
+	}
+	
+	@Override
+	public void onStart() {
+		Agitator.getInstance().setMotorVoltagePercent(percent);
+	}
+}
