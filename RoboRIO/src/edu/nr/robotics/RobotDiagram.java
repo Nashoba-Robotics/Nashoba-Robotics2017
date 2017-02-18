c0004d602cd514d1b57a76d8c480397849025c84
Ben Fox <b2k16@cinderhippo.com>
2017-02-17 16:29:59 -0800

    Robot Diagram additions

--- RoboRIO/src/edu/nr/robotics/RobotDiagram.java
+++ RoboRIO/src/edu/nr/robotics/RobotDiagram.java
@@ -1,5 +1,8 @@
 package edu.nr.robotics;
 
+import edu.nr.robotics.subsystems.hood.Hood;
+import edu.nr.robotics.subsystems.shooter.Shooter;
+import edu.nr.robotics.subsystems.turret.Turret;
 import edu.wpi.first.wpilibj.NamedSendable;
 import edu.wpi.first.wpilibj.tables.ITable;
 
@@ -14,7 +17,9 @@ public class RobotDiagram implements NamedSendable {
 	public void initTable(ITable subtable) {
 		this.table = subtable;
 		if (table != null) {
-			
+			table.putNumber("Shooter Speed", Shooter.getInstance().getSpeed());
+			table.putNumber("Hood Angle", Hood.getInstance().getPosition() * RobotMap.DEGREES_PER_ROTATION);
+			table.putNumber("Turret Angle", Turret.getInstance().getPosition() * RobotMap.DEGREES_PER_ROTATION);
 		}
 		
 	}
