3e2ac70c8c9371b1f4113e6bc7319245c12fa5dc
Ben Fox <b2k16@cinderhippo.com>
2017-02-17 21:06:45 -0800

    Fix merge

--- RoboRIO/src/edu/nr/robotics/multicommands/StopLoaderPukeCommand.java
+++ RoboRIO/src/edu/nr/robotics/multicommands/StopLoaderPukeCommand.java
@@ -2,10 +2,6 @@ package edu.nr.robotics.multicommands;
 
 import edu.nr.lib.commandbased.DoNothingCommand;
 import edu.nr.robotics.subsystems.agitator.Agitator;
-<<<<<<< HEAD
-import edu.nr.robotics.subsystems.agitator.AgitatorRunCommand;
-=======
->>>>>>> a135d3df9ef0bd67f77b7831351401ab0e89c5be
 import edu.nr.robotics.subsystems.loader.LoaderStopCommand;
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
